35142b66479e64471907b6ec04db46d6f6681da1
Emman <you@example.com>
2024-09-16 13:17:05 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <img src="{{ asset('assets/images/logo.svg')}}" alt="" srcset="">
+</body>
+</html>
