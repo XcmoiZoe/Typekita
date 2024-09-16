480cfd760f56f2692ed0f3984e1c30b5cdf2562c
Emman <you@example.com>
2024-09-16 14:12:28 +0800

    bootstrap updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-  <img src="{{ asset('assets/images/logo.svg')}}" alt="" srcset="">
+    <div class="header">
+     <img class="img-fluid" src="{{ asset('assets/images/logo.svg')}}" alt="" srcset="">
+</div>
+<h1>Log In</h1>
+<p>Please login your email id</p>
+ <div class="form">
+<div class="container">
+    <div class="row">
+    <div class="col-sm">
+    
+</div>
+        <div class="col-sm">
+            <label for="Email">Email *</label></div>
+            <div class="col-sm">   <input type="text" placeholder="Email" ></div>
+         
+            <div class="col-sm">
+            <label for="Email">Password *</label></div>
+            <div class="col-sm">   <input type="text" placeholder="Password" ></div>
+    </div>
+</div>
+ </div>
+
+
 </body>
 </html>
